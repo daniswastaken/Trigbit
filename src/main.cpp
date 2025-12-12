64b92a895ac5676c53b4de2937c1e9820b82495e
daniswastaken <daniswastaken@gmail.com>
2025-12-12 21:59:20 +0700

    cp1

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,21 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#define OLED_ADDR   0x3C
+Adafruit_SSD1306 lcd(128, 64, &Wire, -1);
+
 int red = 13;
 
 void setup() {
   pinMode(red, INPUT);
+
+  lcd.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR);
+  lcd.clearDisplay();
+  lcd.setTextColor(WHITE);
+  lcd.setTextSize(2);
+  lcd.setCursor(25, 25); // x y
+  lcd.println("Trigbit");
+  lcd.display();
 }
 
 void loop() {
